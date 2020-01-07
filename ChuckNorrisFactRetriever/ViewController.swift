46c1e2a711f65a1f0318d66ee261624c79b99e51
Pedro Santos Azevedo <pedro_808_@hotmail.com>
2020-01-07 15:31:09 -0300

    Initial Commit

---
+++ ChuckNorrisFactRetriever/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuckNorrisFactRetriever
+//
+//  Created by Pedro Azevedo on 07/01/20.
+//  Copyright © 2020 Pedro Azevedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
